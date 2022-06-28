aa6dd53fe745c461d33460e6ab0cfe8a9d21289f
chuck <cfharr@gmail.com>
2022-06-28 12:24:41 -0700

    cosale: add non-transferable holdings with participation ratio

--- include/seeds.cosale.hpp
+++ include/seeds.cosale.hpp
@@ -12,11 +12,16 @@ using std::string;
    /**
     * The cosale contract supports a collaborative sale of the tendered token (e.g. SEEDS).
     * Participants join the collaborative sale by sending tokens to the cosale contract in
-    * one or more transactions. In return, the participant is issued share-of-sale tokens
-    * (e.g. COSALE) 1:1 to the submitted tokens. These are transferrable tokens based on
-    * the standard eosio.token contract.
+    * one or more transactions. Participants are issued share-of-sale tokens
+    * (e.g. COSALE) in return, 1:1 to the submitted tokens. For general participants these
+    * share_tokens are freely transferrable tokens with the behavior of the standard
+    * eosio.token contract.
     * The share-of-sale tokens have an additional feature that they automatically liquidate
-    * into a cash token (e.g. SEEDUSD) as the tendered tokens are sold.
+    * into cash tokens (e.g. SEEDUSD), over time, as the tendered tokens are sold.
+    * An account (e.g. an institutional account like milest.seeds) may be registered as
+    * as special participant in the collaborative sale whose share_tokens are not
+    * transferrable. A special participant may also be configured to have a sales
+    * participation ratio different from the general participants.
     **/
 
 CONTRACT cosale : public contract {
@@ -69,25 +74,31 @@ CONTRACT cosale : public contract {
       void ontransfer(name from, name to, asset quantity, string memo);
 
       /**
-          * The `payouts` action executed by the manager account distributes
-          *  earnings from the sale in proportion to participant balances.
+          * The `payouts` action executed by the manager account
+          *   (1) distributes earnings from the sale, and
+          *   (2) decrements each participant's balance of tendered tokens,
+          * both in proportion to each participant's balance (weighted by
+          * sales_ratio if applicable).
           *
-          * @param quantity - the total amount (cash tokens) being distributed,
+          * @param payout - the total amount (cash tokens) being distributed,
+          * @param sold - the total amount (tendered tokens) collaboratively sold
       */
-      ACTION payouts(asset quantity);
+      ACTION payouts(asset payout, asset sold);
 
       /**
           * The `payout1` action is a subordinate function to `payouts`, which
           * allows processing a large list of distribution transactions in
           * smaller chunks to accommodate blockchain performance limitations.
           *
-          * @param quantity - the total amount (cash tokens) being distributed,
+          * @param payout - the total amount (cash tokens) being distributed,
+          * @param sold - the total amount (tendered tokens) collaboratively sold
           * @param start - the start index of the first account in this chunk,
           * @param chunksize - the number of payout transactions in this chunk,
-          * @param old_total_balance - ,
+          * @param old_total_balance - TBD,
            
       */
-      ACTION payout1(asset quantity, uint64_t start, uint64_t chunksize, int64_t old_total_balance);
+      ACTION payout1(asset payout, asset sold, 
+                     uint64_t start, uint64_t chunksize, int64_t old_total_balance);
 
       /**
           * The `returns` action executed by the manager account terminates the
@@ -120,6 +131,30 @@ CONTRACT cosale : public contract {
       */
       ACTION transfer(name from, name to, asset quantity, const string& memo);
 
+      /**
+          * The `regspecial` action executed by the manager account
+          * registers a nontransferrable cosale participant. Subsequently,
+          * the participant will be unable to transfer share_tokens. Also,
+          * subsequent payout action will apply a weighting factor to
+          * the account's participation in the sale.
+          *
+          * @param account - the account name,
+          * @param sales_ratio - the proportion at which this account participates
+          *                         in the collaborative sale,
+          * @param memo - memo field
+      */
+      ACTION regspecial(name account, float32 sales_ratio, const string& memo);
+
+      /**
+          * The `unregspecial` action executed by the manager account removes
+          * the special treatment of an account and places it in the general
+          * participant category.
+          *
+          * @param account - the account name,
+          * @param memo - memo field
+      */
+      ACTION unregspecial(name account, const string& memo);
+
 
   private:
 
@@ -136,23 +171,29 @@ CONTRACT cosale : public contract {
        uint64_t primary_key()const { return supply.symbol.code().raw(); }
     };
 
-    TABLE account {
+    TABLE account {  // scoped on account name
       asset    balance;
 
       uint64_t primary_key()const { return balance.symbol.code().raw(); }
     };
 
-    TABLE cosale_config {  // singleton, scoped on token symbol code
+    TABLE cosale_config {  // singleton, scoped on contract
        name            withdrawal_mgr;
        extended_symbol tender_token;
        extended_symbol cash_token;
-       symbol share_token
+       symbol          share_token
+    };
+
+    TABLE special {  // singleton, scoped on account name
+       float32     sales_ratio
     };
 
     typedef eosio::multi_index< "accounts"_n, account > accounts;
     typedef eosio::multi_index< "stat"_n, currency_stats > stats;
     typedef eosio::singleton< "configs"_n, cosale_config > configs;
     typedef eosio::multi_index< "configs"_n, cosale_config >  dump_for_config;
+    typedef eosio::singleton< "specials"_n, special > specials;
+    typedef eosio::multi_index< "specials"_n, special >  dump_for_special;
 
 };
 
